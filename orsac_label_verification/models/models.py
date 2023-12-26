a17513a862a2b546322a3c3204338418f728afe3
Thomas <thomas@vectech.io>
2023-12-26 13:32:08 -0500

    adding se resnet

--- orsac_label_verification/models/models.py
+++ orsac_label_verification/models/models.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 from efficientnet_pytorch import EfficientNet
-from pretrainedmodels import xception
+from pretrainedmodels import xception,se_resnet50
 
 
 class xception_mod(nn.Module):
@@ -27,3 +27,20 @@ class efficientnet_mod(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+# adding se resnet model for use in CUB training.
+# class se_resnet50_mod(nn.Module):
+#     def __init__(self, num_classes, pretrained='imagenet',p=.5):
+#         super().__init__()
+#         model = se_resnet50(pretrained=pretrained)
+#         self.net = model
+
+#         self.net._fc =nn.Identity()
+#         self.dropout=nn.Dropout(p=p)
+#         self.last_linear=nn.Linear(
+#             in_features=self.net._fc.in_features, out_features=num_classes, bias=True
+#         )
+
+#     def forward(self, x):
+#         features=self.net(x)
+#         return self.last_linear(features)
+#         return self.net(x)
